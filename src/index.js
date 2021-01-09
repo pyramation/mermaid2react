1629900bdf0f09f3bd2cff9bedf3589197d3ca26
Dan Lynch <pyramation@gmail.com>
2021-01-09 16:35:08 -0700

    update

--- src/index.js
+++ src/index.js
@@ -1,27 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import { getMermaid, loadMermaid } from './utils';
 
-export class Mermaid extends Component {
-  constructor(props) {
-    super(props);
-    this.onLoad = this.onLoad.bind(this);
-  }
+// export class Mermaid extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.onLoad = this.onLoad.bind(this);
+//   }
 
-  componentDidMount() {
-    if (getMermaid()) {
-      this.onLoad();
-    }
-    loadMermaid(this.onLoad);
-  }
+//   componentDidMount() {
+//     if (getMermaid()) {
+//       this.onLoad();
+//     }
+//     loadMermaid(this.onLoad);
+//   }
 
-  onLoad() {
+//   onLoad() {
+//     window.mermaid.contentLoaded();
+//     this.setState({
+//       loaded: true
+//     });
+//   }
+
+//   render() {
+//     return <div className="mermaid">{this.props.chart}</div>;
+//   }
+// }
+
+export const Mermaid = ({ chart, options }) => {
+  const [loaded, setLoaded] = useState(false);
+
+  const onLoad = () => {
     window.mermaid.contentLoaded();
-    this.setState({
-      loaded: true
-    });
-  }
+    setLoaded(true);
+  };
 
-  render() {
-    return <div className="mermaid">{this.props.chart}</div>;
-  }
-}
+  useEffect(() => {
+    if (getMermaid()) {
+      onLoad();
+    }
+    loadMermaid(onLoad);
+  }, [chart]);
+
+  if (!loaded || !chart) return null;
+  return <div className="mermaid">{chart}</div>;
+};
